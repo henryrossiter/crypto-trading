207593378261d3866ffba56253ee21cfdbd44440
Henry Rossiter <henry.rossiter@utexas.edu>
2020-01-26 12:08:58 -0600

    add notebook

--- api.py
+++ api.py
@@ -21,6 +21,12 @@ def sellCrypto(symbol, usd):
   return resp
 
 def getHistoricals(symbol):
-  resp = r.stocks.get_historicals(symbol)
-  return resp
+  symbol_dict = {
+    'BTC': '3d961844-d360-45fc-989b-f6fca761d511',
+    'ETH': '76637d50-c702-4ed1-bcb5-5b0732a81f48',
+    'LTC': '383280b1-ff53-43fc-9c84-f01afd0989cd',
+    'BCH': '2f2b77c4-e426-4271-ae49-18d5cb296d3a'
+  }
+  data = r.helper.request_get('https://api.robinhood.com/marketdata/forex/historicals/{}/?bounds=24_7&interval=day&span=year'.format(symbol_dict[symbol]))
+  return data
 
